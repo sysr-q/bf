0700498ce44c4bb384aa00b7b0e47d82e89e43a0
Chris Hansen <chris@gibsonsec.org>
2014-02-03 14:33:19 +1100

    It works!

--- bf.go
+++ bf.go
@@ -4,12 +4,14 @@ import (
 	"os"
 	"fmt"
 	"io/ioutil"
+	"container/list"
 )
 
 type Brainfuck struct {
 	Instructions []byte
 	Pointer, At int
-	Memory, Loops []int
+	Memory []int
+	Loops *list.List
 }
 
 func (bf *Brainfuck) PeekInstruction() (next byte) {
@@ -27,47 +29,63 @@ func (bf *Brainfuck) NextInstruction() (next byte) {
 	return
 }
 
+func (bf *Brainfuck) IsZero() (bool) {
+	return bf.Memory[bf.At] == 0
+}
+
 //// BF instructions as functions!
 // +
 func (bf *Brainfuck) Plus() {
-	bf.Memory[bf.At]++
-	return
+	*&bf.Memory[bf.At]++
 }
 
 // -
 func (bf *Brainfuck) Minus() {
-	bf.Memory[bf.At]--
-	return
+	*&bf.Memory[bf.At]--
 }
 
 // >
 func (bf *Brainfuck) Next() {
-	bf.At++
-	return
+	if bf.At >= len(bf.Memory)-1 {
+		*&bf.At = 0
+	} else {
+		*&bf.At++
+	}
 }
 
 // <
 func (bf *Brainfuck) Last() {
-	bf.At--
-	return
+	if bf.At <= 0 {
+		*&bf.At = len(bf.Memory) - 1
+	} else {
+		*&bf.At--
+	}
 }
 
 // [
 func (bf *Brainfuck) Open() {
-	fmt.Print("!")
-	return
+	if bf.IsZero() {
+		// TODO: Skip to closing bracket?
+	} else {
+		// Push pointer to loop stack
+		(*&bf.Loops).PushBack(*&bf.Pointer)
+	}
 }
 
 // ]
 func (bf *Brainfuck) Close() {
-	fmt.Print("?")
-	return
+	if bf.IsZero() {
+		// Pop off the loop stack and continue.
+		(*&bf.Loops).Remove((*&bf.Loops).Back())
+	} else {
+		// Go back to the opening.
+		*&bf.Pointer = (*&bf.Loops).Back().Value.(int)
+	}
 }
 
 // .
 func (bf *Brainfuck) Print() {
 	fmt.Printf("%c", bf.Memory[bf.At])
-	return
 }
 
 
@@ -80,12 +98,14 @@ func main() {
 			Pointer: 0,
 			At: 0,
 			Memory: make([]int, 30),
+			Loops: list.New(),
 		}
 	} else {
 		panic(err) // programming
 	}
 	for next := bf.NextInstruction(); next != 0; next = bf.NextInstruction() {
 		inst := string(next)
+		// TODO: less wasteful to make a map[string]func?
 		if inst == "+" {
 			bf.Plus()
 		} else if inst == "-" {
